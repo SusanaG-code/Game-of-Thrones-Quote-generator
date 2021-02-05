6b3f20cbb7169816e0dac56101a1a0a35abdb650
Susana <su.gonzalez.vejo@gmail.com>
2021-02-05 19:36:20 +0100

    Add box

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import Quotes from "./Quotes";
+import api from "./services/api";
+import "../stylesheets/App.css";
+
+function App() {
+  const [quote, setQuote] = useState("");
+  const [character, setCharacter] = useState("");
+  const [house, setHouse] = useState("");
+
+  useEffect(() => {
+    api.getDataFromApi().then((data) => {
+      setQuote(data.sentence);
+      setCharacter(data.character);
+      setHouse(data.character.house.name);
+    });
+  }, []);
+  const handleClick = () => {
+    api.getDataFromApi().then((data) => {
+      setQuote(data.sentence);
+      setCharacter(data.character);
+      setHouse(data.character.house.name);
+    });
+  };
+
+  return (
+    <div className="App">
+      <div className="main-box">
+        <div className="title-box">
+          <h1 className="title">
+            Choose your Game of Thrones Quote of the day
+          </h1>
+        </div>
+        <Quotes
+          quote={quote}
+          character={character}
+          house={house}
+          handleClick={handleClick}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;

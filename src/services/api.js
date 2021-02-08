eb3d604ac3f7c2ed888d590a73057b1bd194a92b
Susana <su.gonzalez.vejo@gmail.com>
2021-02-08 17:10:48 +0100

    Catch error

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,11 @@
 const getDataFromApi = () => {
-  return fetch("https:game-of-thrones-quotes.herokuapp.com/v1/random")
+  return fetch("https://game-of-thrones-quotes.herokuapp.com/v1/random")
     .then((response) => response.json())
     .then((data) => {
       return data;
+    })
+    .catch((error) => {
+      console.error("Se ha producido un error:", error);
     });
 };
 
